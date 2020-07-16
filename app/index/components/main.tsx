fd1e7a9312c220c1c26291db6f9e4e4156ea9aa2
m84013885 <84013885@qq.com>
2020-07-16 18:48:23 +0800

    fix

--- app/index/components/main.tsx
+++ app/index/components/main.tsx
@@ -4,60 +4,12 @@ import * as style from "./main.css"
 
 import _fetch from '../../utils/fetch'
 import { Toast, Svga, Mask, Loading, Img } from '../../common/index'
-import { useClientRect, useInterval } from './useCommon'
-
-const www: any = [{
-    "step": 1,
-    "content": '我企鹅',
-    "to": 1,
-    "auto": 1,
-    "img": null,
-    "tabs": [
-        {
-            "need": [
-                { "famous": 3 }
-            ],
-            "to": 9,
-            "text": "拜访父亲故交"
-        },
-    ],
-    "tag": [
-        { "lift": 1 }
-    ]
-
-}]
-const wwww: any = [[1], [2, 3]]
+import { useClientRect, useInterval, useRenderTime } from './useCommon'
 
 const Main = () => {
-    // useEffect(() => {
-    //     const fetch = async () => {
-    //         const res = await _fetch({
-    //             url: 'http://172.16.0.133:3000',
-    //             method: 'POST',
-    //             body: {
-    //                 a: wwww,
-    //                 b: www
-    //             }
-    //         })
-    //         console.log(res)
-    //     }
-    //     fetch()
-    // }, [])
-    const intersect = function (nums1: number[], nums2: number[]) {
-        const arr = []
-        const bool = nums1.length === nums2.length ? 3 : nums1.length > nums2.length ? 2 : 1
-        for (let i = 0; i < nums1.length; i++) {
-            for (let j = 0; j < nums2.length; j++) {
-                if (nums1[i] === nums2[j]) {
-                    arr.push(nums1[i])
-                    bool === 2 ? j++ : bool === 1 ? i++ : ''
-                }
-            }
-        }
-        return arr
-    };
     useEffect(() => {
-        console.log(intersect([2, 1], [1, 2]))
+        const time = useRenderTime(new Date('1992/11/20').getTime(), new Date().getTime())
+        console.log(time)
     }, [])
     return (
         <React.Fragment>
--- app/index/components/useCommon.tsx
+++ app/index/components/useCommon.tsx
@@ -63,5 +63,35 @@ function useKeyPress(targetKey: any) {
 
     return keyPressed;
 }
+// 参数时间戳
+function useRenderTime(timeStart: number, timeEnd: number) {
+    const timeStartYear = new Date(timeStart).getFullYear() // 开始年
+    const timeStartMonth = new Date(timeStart).getMonth() + 1 // 开始月
+    const timeStartDay = new Date(timeStart).getDate() // 开始日
 
-export { useClientRect, useInterval, useKeyPress }
+    const timeEndYear = new Date(timeEnd).getFullYear() // 结束年
+    const timeEndMonth = new Date(timeEnd).getMonth() + 1 // 结束月
+    const timeEndDay = new Date(timeEnd).getDate() // 结束日
+
+    let year = 0
+    if (timeStartYear !== timeEndYear) {
+        if (timeStartMonth >= timeEndMonth && timeStartDay >= timeEndDay) {
+            year = timeEndYear - timeStartYear - 1
+            timeStart = new Date(`${timeEndYear - 1}/${timeStartMonth}/${timeStartDay}`).getTime()   // 获取最后年的时间戳
+        } else {
+            year = timeEndYear - timeStartYear
+            timeStart = new Date(`${timeEndYear}/${timeStartMonth}/${timeStartDay}`).getTime()   // 获取最后年的时间戳
+        }
+        
+    }
+    const time = (timeEnd - timeStart) / 1000
+    let day = time > 60 * 60 * 24 ? parseInt(time / (60 * 60 * 24) + '') : 0
+    let hour = time > 60 * 60 ? parseInt(time / (60 * 60) + '') - (day * 24) : 0
+    let minute = time > 60 ? parseInt(time / 60 + '') - (day * 24 * 60) - (hour * 60) : 0
+    let second = parseInt(time + '') - (day * 24 * 60 * 60) - (hour * 60 * 60) - (minute * 60)
+
+    return { year, day, hour, minute, second }
+}
+
+
+export { useClientRect, useInterval, useKeyPress, useRenderTime }
