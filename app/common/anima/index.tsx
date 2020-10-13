6e6e5b149657c01c56ca80f5603ed1989b54964f
m84013885 <84013885@qq.com>
2020-10-13 16:45:10 +0800

    fix

--- app/common/anima/index.tsx
+++ app/common/anima/index.tsx
@@ -2,7 +2,16 @@ import * as React from "react";
 import { useEffect, useState } from "react";
 import * as style from "./index.css"
 
-const arrImg: any = []
+let canvas: any = null
+let ctx: any = null
+let canvasContent: any = null
+
+// 图片播放数字
+let number: number = 0
+// 图片加载数字
+let imageLoadNum = 0
+// 加载完的数组
+let arrImage: any[] = []
 
 const requestAnimFrame = (function () {
     return window.requestAnimationFrame ||
@@ -21,27 +30,61 @@ interface prop {
 
 const Anima = (prop: prop) => {
     const { imgNumber, path, play, callback } = prop
-    let number: number = 0
-    for (let i = 0; i < imgNumber; i++) {
-        arrImg.push(require(`../../${path}${i}.anima.png`).default)
+    const [imageOnLoad, setImageOnLoad] = useState(0)
+
+    const _reset = () => {
+        number = 0
+        imageLoadNum = 0
+        arrImage = []
     }
-    const [num, setNum] = useState(0)
     const step = () => {
         if (number < imgNumber) {
-            setNum(number++)
+            drawImgae()
+            number++
             requestAnimFrame(step)
         } else {
+            _reset()
             callback && callback()
         }
     }
+    const drawImgae = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        ctx.drawImage(arrImage[number], 0, 0, canvas.width, canvas.height)
+    }
+    // 图片加载
+    const onImageLoad = () => {
+        for (let i = 0; i < imgNumber; i++) {
+            const url = require(`../../${path}${i}.anima.png`)
+            const img = new Image()
+            img.src = url.default
+            arrImage.push(img)
+            img.onload = () => {
+                setImageOnLoad(i)
+            }
+        }
+    }
+    useEffect(() => {
+        imageLoadNum++
+        if (imageLoadNum === imgNumber) {
+            // 加载完成
+            requestAnimFrame(step)
+        }
+    }, [imageOnLoad])
+
+    useEffect(() => {
+        ctx = canvas.getContext('2d')
+        // 初始化canvas长宽
+        canvas.width = parseInt(getComputedStyle(canvasContent).width)
+        canvas.height = parseInt(getComputedStyle(canvasContent).height)
+    }, [])
     useEffect(() => {
         if (play) {
-            requestAnimFrame(step)
+            onImageLoad()
         }
     }, [play])
     return (
-        <div className={play ? style.imageContent : style.none}>
-            <img className={style.image} src={arrImg[num]} />
+        <div className={play ? style.imageContent : style.opacity} ref={(dom) => { canvasContent = dom }}>
+            <canvas className={style.image} ref={(dom) => { canvas = dom }}></canvas>
         </div>
     )
 }
--- app/index/components/main.tsx
+++ app/index/components/main.tsx
@@ -6,13 +6,30 @@ import _fetch from '../../utils/fetch'
 import { Toast, Svga, Mask, Loading, Img, Msg, Video, Swiper, Anima } from '../../common/index'
 import { useInterval, useKeyPress, useRenderTime } from '../../common/useCommon'
 
+const animaNow = [
+    { path: 'index/assets/anima/', num: 125 },
+    { path: 'index/assets/anima1/', num: 160 }
+]
+
 const Main = () => {
+    const [now, setNow] = useState(0)
     const [anima, setAnima] = useState(false)
     return (
         <React.Fragment>
-            <div className={style.scrollView} onClick={() => { setAnima(true) }}>
+            <div className={style.scrollView}>
+                <div className={style.btn} onClick={() => { setAnima(true) }}>
+                    播放动画(目前是{now ? '2' : '1'})
+                </div>
+                <div className={style.btn}>
+                    <div onClick={() => { setNow(0) }}>选择1</div>
+                    <div onClick={() => { setNow(1) }}>选择2</div>
+                </div>
+
                 {/* something */}
-                <Anima imgNumber={125} path='index/assets/anima/' play={anima} callback={() => { console.log('end') }} />
+                <div className={style.animaContent}>
+                    <Anima imgNumber={animaNow[now].num} path={animaNow[now].path} play={anima} callback={() => { setAnima(false) }} />
+                </div>
+
             </div>
             <Loading />
             <Mask>
