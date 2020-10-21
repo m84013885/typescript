56bc786b7edd0ca0cd8296e08498d01ccd0e2b1a
m84013885 <84013885@qq.com>
2020-10-21 18:56:16 +0800

    fix

--- app/index/components/main.tsx
+++ app/index/components/main.tsx
@@ -5,11 +5,13 @@ import _fetch from '../../utils/fetch'
 import { Toast, Svga, Mask, Loading, Img, Msg, Video, Swiper, Anima } from '../../common/index'
 import { useInterval, useKeyPress, useRenderTime } from '../../common/useCommon'
 
+const mp4 = require('../assets/482715dbbcc.mp4').default
+
 const Main = () => {
     return (
         <React.Fragment>
             <div className={style.scrollView}>
-                
+                <Video src={mp4}/>
             </div>
             <Loading />
             <Mask>
